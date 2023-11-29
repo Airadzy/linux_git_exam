2244ea175ceb395874178cac14b461c4f37b7eee
Adrian <airadzy@gmail.com>
2023-11-29 10:27:41 +0100

    ran the bash script to create the files

--- text_ops.sh
+++ text_ops.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-$1
-FUNCTION_SEARCH=$(grep "function" | wc -l)
-echo FUNCTION_SEARCH > output/unique-headers.txt
+# count occurance of keyword function in a given file
+
+FUNCTION_SEARCH=$(grep -o "function" "$1" | wc -l)
+
+echo "FUNCTION_SEARCH Result:$FUNCTION_SEARCH" > output/function-keyword-count.txt
+
+EXTRACT_HEADERS=$(grep -o "^#.*" "$1" | sed "s/^#+ //"| sed 's/^#//; s/^##//')
+
+echo "EXTRACT_HEADERS Result:$EXTRACT_HEADERS" > output/unique-headers.txt
+
+NUMERICAL_VALUES=$(grep -o "[0-9]\+" "$1" | sort -n)
+
+echo "$NUMERICAL_VALUES" > output/sorted-numbers.txt
+
+FEATURE_ITEMS=$(grep -A 8 "^# FEATURES" "$1" | tr -d "\n"| cat)
+
+echo "FEATURE_ITEMS Result:$FEATURE_ITEMS" > output/features-summary.txt
+
+FIND_CALCULATOR=$(grep "calculator" "$1" | sort -r)
+
+echo "FIND_CALCULATOR Result:$FIND_CALCULATOR" > output/calculator-lines.txt
+
+for file in output/*txt; do
+	cat "$file"
+done
+
